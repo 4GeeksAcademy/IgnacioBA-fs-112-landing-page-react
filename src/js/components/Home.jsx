a53009b66b722643e9b45c561eb7177b075c47e5
IgnacioBA9 <150173378+IgnacioBA9@users.noreply.github.com>
2025-06-09 10:40:42 +0000

    [feat:]New components added, Navbar, Jumbotron, Card(4)

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,42 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap"
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<div>
+			<Navbar />
+			<div className="container">
+				<div className="bg-light p-4 rounded m-4 mx-auto" style={{ maxWidth: "1300px" }}>
+					<Jumbotron />
+				</div>
+			</div>
+			<div className="container mt-4 d-flex flex-row mx-auto" style={{ maxWidth: "1300px" }}>
+				<div className="card1  mx-3">
+					<Card />
+				</div>
+				<div className="card2  mx-3">
+					<Card />
+				</div>
+				<div className="card3  mx-3">
+					<Card />
+				</div>
+				<div className="card4  mx-3">
+					<Card />
+				</div>
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
 		</div>
+
 	);
 };
 
